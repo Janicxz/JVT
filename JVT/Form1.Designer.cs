2154c2e7d0c8accb91e7b04d0fffcc02fd856298
Janizu99 <janizu99@gmail.com>
2019-09-15 23:32:38 +0300

    Add project files.

---
+++ JVT/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace JVT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelPlayer = new System.Windows.Forms.Panel();
+            this.buttonPlayerStop = new System.Windows.Forms.Button();
+            this.hScrollBarPlayer = new System.Windows.Forms.HScrollBar();
+            this.trackBarPlayer = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarPlayer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelPlayer
+            // 
+            this.panelPlayer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panelPlayer.Location = new System.Drawing.Point(0, 0);
+            this.panelPlayer.Name = "panelPlayer";
+            this.panelPlayer.Size = new System.Drawing.Size(800, 387);
+            this.panelPlayer.TabIndex = 0;
+            // 
+            // buttonPlayerStop
+            // 
+            this.buttonPlayerStop.Location = new System.Drawing.Point(344, 421);
+            this.buttonPlayerStop.Name = "buttonPlayerStop";
+            this.buttonPlayerStop.Size = new System.Drawing.Size(75, 23);
+            this.buttonPlayerStop.TabIndex = 1;
+            this.buttonPlayerStop.Text = "Pause";
+            this.buttonPlayerStop.UseVisualStyleBackColor = true;
+            this.buttonPlayerStop.Click += new System.EventHandler(this.ButtonPlayerStop_Click);
+            // 
+            // hScrollBarPlayer
+            // 
+            this.hScrollBarPlayer.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.hScrollBarPlayer.LargeChange = 2;
+            this.hScrollBarPlayer.Location = new System.Drawing.Point(9, 390);
+            this.hScrollBarPlayer.Name = "hScrollBarPlayer";
+            this.hScrollBarPlayer.Size = new System.Drawing.Size(782, 19);
+            this.hScrollBarPlayer.TabIndex = 2;
+            this.hScrollBarPlayer.Scroll += new System.Windows.Forms.ScrollEventHandler(this.HScrollBarPlayer_Scroll);
+            // 
+            // trackBarPlayer
+            // 
+            this.trackBarPlayer.Location = new System.Drawing.Point(476, 398);
+            this.trackBarPlayer.Maximum = 100;
+            this.trackBarPlayer.Name = "trackBarPlayer";
+            this.trackBarPlayer.Size = new System.Drawing.Size(104, 45);
+            this.trackBarPlayer.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 441);
+            this.Controls.Add(this.trackBarPlayer);
+            this.Controls.Add(this.hScrollBarPlayer);
+            this.Controls.Add(this.buttonPlayerStop);
+            this.Controls.Add(this.panelPlayer);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarPlayer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelPlayer;
+        private System.Windows.Forms.Button buttonPlayerStop;
+        private System.Windows.Forms.HScrollBar hScrollBarPlayer;
+        private System.Windows.Forms.TrackBar trackBarPlayer;
+    }
+}
+
---
+++ JVT/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Mpv.NET.Player;
+
+namespace JVT
+{
+    public partial class Form1 : Form
+    {
+        string dllPath = "lib\\mpv-1.dll";
+        MpvPlayer player;
+        Timer timerScrollBarUpdate;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Init
+            player = new MpvPlayer(panelPlayer.Handle, dllPath);
+            player.Load("test.mp4");
+            player.Loop = true;
+            player.Resume();
+            player.MediaLoaded += Player_MediaLoaded;
+            player.MediaPaused += Player_MediaPaused;
+            player.MediaResumed += Player_MediaResumed;
+
+            timerScrollBarUpdate = new Timer();
+            timerScrollBarUpdate.Interval = 100;
+            timerScrollBarUpdate.Tick += TimerScrollBarUpdate_Tick;
+            timerScrollBarUpdate.Start();
+        }
+
+        private void Player_MediaResumed(object sender, EventArgs e)
+        {
+            Console.WriteLine("playback resumed");
+            timerScrollBarUpdate.Start();
+        }
+
+        private void Player_MediaPaused(object sender, EventArgs e)
+        {
+            Console.WriteLine("Playback paused.");
+            timerScrollBarUpdate.Stop();
+        }
+
+        private void TimerScrollBarUpdate_Tick(object sender, EventArgs e)
+        {
+            //Console.WriteLine("timer Pos {0}, dur {1}", player.Position, player.Duration);
+            hScrollBarPlayer.Invoke((Action)delegate
+            {
+                hScrollBarPlayer.Value = (int)player.Position.TotalMilliseconds;
+                //Application.DoEvents();
+            });
+        }
+
+        private void Player_PositionChanged(object sender, MpvPlayerPositionChangedEventArgs e)
+        {
+            hScrollBarPlayer.Invoke((Action)delegate
+            {
+                hScrollBarPlayer.Value = (int)player.Position.TotalMilliseconds;
+                //Application.DoEvents();
+            });
+        }
+
+
+        private void Player_MediaLoaded(object sender, EventArgs e)
+        {
+            Console.WriteLine("loaded media Dur:" + player.Duration.TotalMilliseconds);
+            hScrollBarPlayer.Invoke((Action)delegate
+            {
+                hScrollBarPlayer.Maximum = (int)player.Duration.TotalMilliseconds;
+                hScrollBarPlayer.Minimum = 0;
+            });
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+             if(player != null) player.Dispose();
+        }
+
+        bool playerPaused = false;
+        private void ButtonPlayerStop_Click(object sender, EventArgs e)
+        {
+            togglePlayerPause();
+           // Console.WriteLine("Playing {0}", player.IsPlaying);
+           /* if (!playerPaused)
+            {
+                player.Pause();
+                buttonPlayerStop.Text = "Resume";
+                playerPaused = true;
+            }
+            else
+            {
+                player.Resume();
+                buttonPlayerStop.Text = "Pause";
+                playerPaused = false;
+            }*/
+        }
+
+        private void togglePlayerPause()
+        {
+            if (player.IsPlaying)
+            {
+                player.Pause();
+                buttonPlayerStop.Invoke((Action)delegate
+                {
+                    buttonPlayerStop.Text = "Resume";
+                });
+            }
+            else
+            {
+                player.Resume();
+                buttonPlayerStop.Invoke((Action)delegate
+                {
+                    buttonPlayerStop.Text = "Pause";
+                });
+            }
+        }
+
+        private void HScrollBarPlayer_Scroll(object sender, ScrollEventArgs e)
+        {
+            timerScrollBarUpdate.Stop();
+            Console.WriteLine("scrolled");
+           // player.SeekAsync(TimeSpan.FromMilliseconds(e.NewValue));
+            timerScrollBarUpdate.Start();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            Console.WriteLine("Keydown: {0}", e.KeyCode);
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    player.PreviousFrame();
+                    e.Handled = true;
+                    break;
+                case Keys.Right:
+                    player.NextFrame();
+                    e.Handled = true;
+                    break;
+                case Keys.Space:
+                    togglePlayerPause();
+                    e.Handled = true;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
